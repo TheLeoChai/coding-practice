da6a19e65dd8481d2532926badd4d0e08f622dad
TheLeoChai <43290645+TheLeoChai@users.noreply.github.com>
2019-12-06 12:11:04 -0500

    aiya

--- bundle/dpc.cpp
+++ bundle/dpc.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-int inp[3][10005];
+int inp[3][100005];
 long long dp[3][100005];
-
+/*
 long long day(int d, int p){
-    if(d >= n-1){
+    if(d >= n){
         dp[p%3][d] = 0;
         dp[(p+1)%3][d] = 0;
         dp[(p+2)%3][d] = 0;
@@ -23,13 +23,18 @@ long long day(int d, int p){
 
     return max(dp[(p+1)%3][d], dp[(p+2)%3][d]);
 }
-
+*/
 int main(){
     memset(dp,-1,sizeof(dp));
     cin >> n;
-    for(int i=0;i<n;i++){
+    for(int i=1;i<=n;i++){
         cin >> inp[0][i] >> inp[1][i] >> inp[2][i];
+        for(int j=0;j<3;j++){
+            dp[j][i]= max(dp[(j+1)%3][i-1],dp[(j+2)%3][i-1]) + inp[j][i];
+            if(i-1 == 0)
+                dp[j][i] = 0 + inp[j][i];
+        }
     }
-    cout << day(0,3);
+    cout << max(dp[0][n],max(dp[1][n],dp[2][n]));
     return 0;
 }
