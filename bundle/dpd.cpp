ebb561871d8d77e72335dc66d29c8c1091928437
TheLeoChai <43290645+TheLeoChai@users.noreply.github.com>
2019-12-09 12:31:46 -0500

    dpe in dpd

--- bundle/dpd.cpp
+++ bundle/dpd.cpp
@@ -4,19 +4,30 @@ using namespace std;
 int N, W;
 int v[105];
 int w[105];
-long long dp[100005][105];
+int dp[100005][105];
+int T = 0;
 
 int main() {
     cin >> N >> W;
     for (int i = 1; i <= N; i++) {
         cin >> w[i] >> v[i];
+        T+= v[i];
     }
+    memset(dp,0x3f,sizeof(dp));
+    dp[0][0] = 0;
     for (int j = 1; j <= N; j++) {
-        for (int i = 0;i < w[j];i++)
+        for (int i = 0;i < v[j];i++)
             dp[i][j] = dp[i][j - 1];
-        for (int i = w[j];i <= W;i++)
-            dp[i][j] = max(dp[i][j - 1], dp[i - w[j]][j - 1] + v[j]);
+        for (int i = v[j];i <= T;i++)
+            dp[i][j] = min(dp[i][j - 1], dp[i - v[j]][j - 1] + w[j]);
+        //cout << endl;
     }
-    cout << dp[W][N];
+    int val = 0;
+    for(int i=0;i<=T;i++){
+        if(dp[i][N] <= W){
+            val = max(val, i);
+        }
+    }
+    cout << val;
     return 0;
 }
